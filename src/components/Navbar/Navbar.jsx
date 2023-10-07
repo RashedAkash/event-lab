bf5db0651bef3ee7c122ad8da1549f481740c787
hablu <131282586+hablu@users.noreply.github.com>
2023-10-07 07:52:25 +0600

    navbar create and responsive complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { FirebaseContext } from '../../Context/AuthContext';
+import logo from '../../assets/logo (1).png';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   const { name } = useContext(FirebaseContext);
@@ -13,36 +15,29 @@ const Navbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='about'>About</NavLink></li>
+        <li><NavLink to='blog'>Blog</NavLink></li>
+        <li><NavLink to='contact'>Contact</NavLink></li>
+        
+        
       </ul>
     </div>
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <NavLink to='/' className="btn btn-ghost normal-case text-xl"><img src={logo} alt="" /></NavLink>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li tabIndex={0}>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+      <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='about'>About</NavLink></li>
+        <li><NavLink to='blog'>Blog</NavLink></li>
+        <li><NavLink to='contact'>Contact</NavLink></li>
+      
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <NavLink className="btn bg-pink-700 text-white">Login</NavLink>
+    <NavLink className="hidden md:block py-3 px-3 rounded-lg font-semibold ml-3 bg-pink-700 text-white">Sign up</NavLink>
+    
   </div>
 </div>
     </div>

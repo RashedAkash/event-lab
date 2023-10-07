8a769e7a7a6842a9d2e4b69fbbf4fe64f266c8b7
hablu <131282586+hablu@users.noreply.github.com>
2023-10-07 09:55:31 +0600

    team area create and responsive complete

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Price from '../../components/Price/Price';
+import Team from '../../components/Team/Team';
 
 const Home = () => {
   return (
     <div>
+      <Team />
       <Price />
+      
       
     </div>
   );
